3e3782677858b525b3479516ae5baf6201668b59
Alina Chagan <alina.chagan@yandex.ru>
2022-12-23 19:34:42 +0300

    11 lab

---
+++ Lab11/TestFor92/ButInput.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFor9
+{
+    internal class ButInput
+    {
+        private IWebDriver driver;
+
+        public ButInput(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public ButInput ButInputButton()
+        {
+            driver.FindElement(By.XPath("/html/body/div/div[2]/div[2]/div[2]/div/div/form/div/input[1]")).Click();
+            return new ButInput(driver);
+        }
+    }
+}
---
+++ Lab11/TestFor92/CreateMaterial.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFor92
+{
+    internal class CreateMaterial
+    {
+        private IWebDriver driver;
+
+        public CreateMaterial(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public CreateMaterial CreateMaterialButton()
+        {
+            driver.FindElement(By.XPath("/html/body/div/div[2]/div[2]/div[3]/div/div/ul/li[6]/a")).Click();
+            return new CreateMaterial(driver);
+        }
+    }
+
+}
